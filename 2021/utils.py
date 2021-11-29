7ca3b1ff42dc0273db4d715bb2fdcf9df9d92796
hkve <haakon.kvernmoen@gmail.com>
2021-11-29 14:00:27 +0100

    .

--- 2021/utils.py
+++ 2021/utils.py
@@ -1,1 +1,21 @@
-# todo
+import re
+import os
+
+
+def read_file(filename, pat=None):
+	folder = "input"
+
+	if not folder in os.listdir():
+		os.mkdir(folder)
+		exit()
+
+	if not filename in os.listdir(folder):
+		print(f"Did not find {filename}")
+		exit()
+
+	with open(f"{folder}/{filename}", "r") as file:
+		for line in file:		
+			if pat is not None:
+				yield re.findall(pat, line)	
+			else:
+				yield line.strip()
